5d90e79299b939e741bb724bdeffbacdf9c1a969
Anish <anishmandalme1234@outlook.com>
2024-02-20 21:09:42 +0530

    Remove dependencies from rust calculator

--- rust/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub fn uppercase_first_letter(s: String) -> String {
-    let mut s_as_vec: Vec<char> = s.chars().collect();
-    s_as_vec[0] = s_as_vec[0].to_uppercase().nth(0).unwrap();
-
-    let result: String = s_as_vec.into_iter().collect();
-    result
-}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,132 +1,83 @@
-mod lib;
-use crate::lib::uppercase_first_letter;
-use colored::*;
-use inquire;
-use std::io::Result;
+use std::io::{self, Write};
 
-fn intro() {
+fn main() -> Result<(), ()> {
+    let mut answer_buffer: String;
+    let number1: i32;
+    let number2: i32;
+
+    println!("Welcome to 2 numbers adding calculator :)");
+    println!(
+        "Please select the number in () for the required operation to be done:
+(1) Addition
+(2) Subtraction
+(3) Multiplication
+(4) Division"
+    );
+
     loop {
-        let name = inquire::Text::new("What is your name ?")
-            .with_default("User")
-            .prompt();
+        answer_buffer = String::new();
 
-        match name {
-            Ok(val) => {
-                println!(
-                    "Welcome to my calculator, {}\n",
-                    uppercase_first_letter(val)
-                );
+        print!("Enter option number: ");
+        let _ = io::stdout().flush(); // flush the output first
+        io::stdin()
+            .read_line(&mut answer_buffer)
+            .expect("Could not take input for option number.");
+
+        if let "1" | "2" | "3" | "4" = answer_buffer.trim() {
+            break;
+        }
+        println!("Please enter the correct value :(");
+    }
+
+    loop {
+        let mut input = String::new();
+
+        print!("Enter the first number: ");
+        let _ = io::stdout().flush(); // flush the output first
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Could not take input for the first number.");
+
+        match input.trim().parse::<i32>() {
+            Ok(v) => {
+                number1 = v;
                 break;
             }
-            Err(_) => {
-                eprintln!(
-                    "Something went wrong while your name input, no worry put your name again."
-                );
-                continue;
-            }
+            Err(_) => println!("Please enter a valid number."),
         }
     }
-}
 
-fn operator_selection() -> String {
     loop {
-        let operations = vec!["Addition", "Substraction", "Multiplication", "Division"];
-        let operator =
-            inquire::Select::new("What Operation you would wish to do ?", &operations).prompt();
+        let mut input = String::new();
 
-        match operator {
-            Ok(val) => return val.to_string(),
-            Err(_) => {
-                eprintln!("Something went wrong while taking the input for which operation you want to do but no worry you can input it again.");
-                continue;
-            }
-        }
-    }
-}
+        print!("Enter the second number: ");
+        let _ = io::stdout().flush(); // flush the output first
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Could not take input for the second number.");
 
-fn asking_num2() -> f32 {
-    loop {
-        let num2 = inquire::Text::new("What is the second number ?")
-            .with_default("1")
-            .prompt();
-
-        match num2 {
-            Ok(val) => {
-                let num2: f32 = match val.trim().parse() {
-                    Ok(val) => val,
-                    Err(_) => {
-                        eprintln!("Wrong Input.");
+        match input.trim().parse::<i32>() {
+            Ok(v) => {
+                if answer_buffer.trim() == "4" {
+                    if v == 0 {
+                        println!("Denominator can't be zero");
                         continue;
                     }
-                };
-                return num2;
+                }
+                number2 = v;
+                break;
             }
-            Err(_) => {
-                eprintln!("Failed to take input of your second number.");
-                continue;
-            }
+            Err(_) => println!("Please enter a valid number."),
         }
     }
-}
 
-fn asking_num1() -> f32 {
-    loop {
-        let num1 = inquire::Text::new("What is the first number ?")
-            .with_default("1")
-            .prompt();
-
-        match num1 {
-            Ok(val) => {
-                let num1: f32 = match val.trim().parse() {
-                    Ok(val) => val,
-                    Err(_) => {
-                        eprintln!("Wrong Input.");
-                        continue;
-                    }
-                };
-                return num1;
-            }
-            Err(_) => {
-                eprintln!("Failed to take your input for first number.");
-                continue;
-            }
-        }
-    }
-}
-
-fn main() -> Result<()> {
-    println!(
-        "Welcome to my calculator in {}\n",
-        "rust".on_truecolor(255, 165, 0)
-    );
-    println!("Before you can use the {}", "calculator".underline());
-
-    intro();
-    let operation = operator_selection();
-    let num1 = asking_num1();
-    let num2 = asking_num2();
-
-    if operation == "Addition" {
-        println!("Result: {} + {} = {}", num1, num2, num1 + num2);
-    } else if operation == "Substraction" {
-        println!("Result: {} - {} = {}", num1, num2, num1 - num2)
-    } else if operation == "Multiplication" {
-        println!("Result: {} * {} = {}", num1, num2, num1 * num2);
-    } else if operation == "Division" {
-        if num2 == 0.0 {
-            println!("Anything divided by 0 will result in undefined");
-
-            return Ok(());
-        }
-        println!(
-            "Result: {} / {} = {}",
-            num1,
-            num2,
-            num1 as f32 / num2 as f32
-        );
-    }
-
-    println!("Thanks for using my calculator in rust");
+    match answer_buffer.trim() {
+        "1" => println!("{} + {} = {}", number1, number2, number1 + number2),
+        "2" => println!("{} - {} = {}", number1, number2, number1 - number2),
+        "3" => println!("{} * {} = {}", number1, number2, number1 * number2),
+        "4" => println!("{} / {} = {}", number1, number2, number1 / number2),
+        _ => (),
+    };
 
     Ok(())
 }
